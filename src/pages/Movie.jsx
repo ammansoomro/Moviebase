a94b59023a4846843bae455fa13c9204dbbc551c
Amman Soomro <63865428+ammansoomro@users.noreply.github.com>
2025-04-11 21:02:05 +0500

    Removed the use of Styled Components

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,36 +1,34 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import styled from 'styled-components';
+import './Movie.scss'; // Import the CSS file
+
 function Movie() {
   const [item, setItem] = useState([]);
   const [genre, setgenre] = useState([]);
   const [description, setdescription] = useState(" ");
   const [torrents, settorrents] = useState([]);
   let param = useParams();
+
   useEffect(() => {
     const getItem = async (id) => {
       const response = await fetch(
         `https://api.allorigins.win/raw?url=https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
       ).then((response) => response.json());
       setItem(response.data.movie);
-      setgenre(response.data.movie.genres)
-      setdescription(response.data.movie.description_full)
+      setgenre(response.data.movie.genres);
+      setdescription(response.data.movie.description_full);
       settorrents(response.data.movie.torrents);
     };
     getItem(param.id);
   }, [param.id]);
 
-
   return (
-    <Wrapper>
-      <MovieBanner>
-        <Image>
-          <img
-            src={item.background_image_original}
-            alt={item.title}
-          />
-        </Image>
-        <Container>
+    <div className="wrapper">
+      <div className="movie-banner">
+        <div className="image">
+          <img src={item.background_image_original} alt={item.title} />
+        </div>
+        <div className="container">
           <div className="title-container">
             <div className="title-top">
               <div className="movie-title">
@@ -56,259 +54,46 @@ function Movie() {
                 <div>
                   <strong>Genres: </strong>
                   <ul>
-                    {genre.map((listitem) => {
-                      return (
-                        <li key={listitem}>{listitem} |</li>
-                      )
-                    })}
+                    {genre.map((listitem) => (
+                      <li key={listitem}>{listitem} |</li>
+                    ))}
                   </ul>
                 </div>
                 <div>
-                  {torrents.map((item) => {
-                    return (
-                      <a href={item.url} key={item.url}>
-                        <button className="btn">{item.quality}.{item.type.charAt(0).toUpperCase() + item.type.slice(1)}</button>
-                      </a>
-                    )
-                  })}
-
+                  {torrents.map((item) => (
+                    <a href={item.url} key={item.url}>
+                      <button className="btn">
+                        {item.quality}.{item.type.charAt(0).toUpperCase() + item.type.slice(1)}
+                      </button>
+                    </a>
+                  ))}
                 </div>
               </div>
             </div>
           </div>
-        </Container>
-        <Card>
-          <CardImage>
+        </div>
+        <div className="movie-card">
+          <div className="card-image">
             <img src={item.medium_cover_image} alt="Please Wait, Fetching Data..." />
-          </CardImage>
-        </Card>
-      </MovieBanner>
-      <MovieDetails>
-        <Trailer>
+          </div>
+        </div>
+      </div>
+      <section className="movie-details">
+        <section className="trailer">
           <strong>Trailer</strong>
-          <iframe width="660" height="350" src={"https://www.youtube-nocookie.com/embed/" + item.yt_trailer_code} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-        </Trailer>
-      </MovieDetails>
-    </Wrapper>
-
+          <iframe
+            width="660"
+            height="350"
+            src={`https://www.youtube-nocookie.com/embed/${item.yt_trailer_code}`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </section>
+      </section>
+    </div>
   );
 }
 
-const Wrapper = styled.div`
-  margin: 0rem 1rem;
-`;
-
-const MovieBanner = styled.div`
-  width: 100%;
-  margin: 10px auto;
-  max-width: 1200px;
-  min-height: 520px;
-  max-height: 520px;
-  padding: 70px 50px;
-  position: relative;
-  display: flex;
-  justify-content:center;
-
-  &::after {
-    content: "";
-    position: absolute;
-    left: 0px;
-    top: 0px;
-    width: 100%;
-    height: 100%;
-    background: rgba(32, 32, 32, 0.7);
-    background: linear-gradient(0deg, rgb(8, 8, 8) 0%, rgba(0, 0, 0, 0.6) 100%);
-  }
-`;
-
-const Image = styled.div`
-  position: absolute;
-  left: 0px;
-  top: 0px;
-  width: 100%;
-  height: 100%;
-  border-radius: 5px;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    object-position: center;
-  }
-
-  &::before {
-    content: "";
-    position: absolute;
-    left: 0px;
-    top: 0px;
-    width: 100%;
-    height: 100%;
-    background: rgba(32, 32, 32, 0.7);
-    background: linear-gradient(
-      -90deg,
-      rgba(37, 37, 37, 0.2) 0%,
-      rgba(15, 15, 15, 0.3) 100%
-    );
-  }
-`;
-
-const Container = styled.div`
-  z-index: 100;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  width: 100%;
-  position: relative;
-
-  .title-container {
-    width: 100%;
-    max-width: 500px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
-
-  .title-container p{
-    position: relative;
-    margin: 1.5rem 0;
-    color: #747474;
-    width: 180%;
-    text-align: justify;
-}
-  .movie-title h1 {
-    color: #ffffff;
-    font-weight: 600;
-    font-size: 2.3rem;
-    font-family: "Blanka", sans-serif;
-    line-height: 40px;
-    letter-spacing: 1px;
-  }
-  .about {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    margin-top: 10px;
-    flex-wrap: wrap;
-    color: #e9e9e9;
-  }
-  .rating {
-    display: flex;
-    align-items: center;
-    justify-content: flex;
-    letter-spacing: 0.1rem;
-    img {
-      height: 2rem;
-      width: 2.4rem;
-      object-fit: contain;
-      object-position: center;
-      margin: 0px 10px;
-    }
-  }
-  .about span {
-    margin-right: 1.1rem;
-    letter-spacing: 1px;
-    font-size: 0.8rem;
-  }
-
-  .title-bottom{
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    flex-wrap: wrap;
-  }
-  .btn {
-    border: none;
-    outline: 2px solid #d3060a;
-    background: transparent;
-    color: #d3060a;
-    cursor: pointer;
-    font-weight: bold;
-    padding: 5px 4px;
-    transition: 0.4s;
-    margin-top: 1rem;
-    margin-right: 0.9rem;
-  }
-  .btn:hover{
-    color: #a30507;
-    outline: 2px solid #a30507;
-  }
-  li{
-    float: left;
-    display: block;
-  color: white;
-  text-align: center;  
-  font-weight: bold;
-    color: #d3060a;
-    font-size: 0.8rem;
-}
-
-li:not(:first-child){
-      padding: 0rem 0.2rem;
-  }
-  .category{
-    margin-right: 40px;
-    font-size: 1.2rem;
-    display: flex;
-    flex-direction: column;
-}
-.category strong{
-    font-weight: 500;
-    color: #ffffff;
-  }
-`;
-
-
-
-const MovieDetails = styled.section`
-width: 100%;
-max-width: 1000px;
-margin: 2rem auto 1rem auto;
-padding: 2rem;
-display: flex;
-flex-direction: column;
-text-align: center;
-p{
-    margin: 1rem 0;
-    color: #747474;
-}
-`;
-
-const Trailer = styled.section`
-width: 100%;
-max-width: 800px;
-margin: 0rem auto 3.5rem auto;
-display: flex;  
-justify-content: flex-start;
-align-items:center;
-flex-direction: column;
-strong{
-    /* color: #d3060a; */
-    color: #FFFFFF;
-    font-family: "Blanka", sans-serif;
-    font-size: 1.5rem;
-    font-weight: 400;
-    padding: 1.2rem;
-}
-`;
-const Card = styled.div`
-  z-index: 2;
-  width: 300px;
-  height: 350px;
-  position: relative;
-  border-radius: 1rem;
-  margin-top: 1rem;
-`;
-
-// ==========  Card Image ==========
-const CardImage = styled.div`
-  width: 100%;
-  height: 100%;
-  border-radius: 1rem;
-  img {
-    border-radius: 1rem;
-    width: 100%;
-    height: 100%;
-    object-fit: fill;
-    object-position: center;
-  }
-`;
 export default Movie;
--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,294 +1,98 @@
-import styled from "styled-components";
 import { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import { useParams } from 'react-router-dom';
 import ReactPaginate from 'react-paginate';
-import SearchBar from "../components/MainSearch"
+import SearchBar from "../components/MainSearch";
+import "./Searched.scss";
 
-function AllMovies() {
+function Searched() {
+  const [Searched, setSearched] = useState([]);
+  const [MovieCount, setMovieCount] = useState(0);
+  let param = useParams();
 
+  useEffect(() => {
+    const getFeaturedMovies = async (search) => {
+      const url = `https://yts.mx/api/v2/list_movies.json?query_term=${search}`;
+      const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`).then((response) => response.json());
+      let data = JSON.parse(response.contents);
+      setSearched(data.data.movies);
+      setMovieCount(data.data.movie_count);
+    };
+    getFeaturedMovies(param.search);
+  }, [param.search]);
 
+  const fetchMovies = async (Search, CurrentPage) => {
+    const url = `https://yts.mx/api/v2/list_movies.json?query_term=${Search}&limit=20&page=${CurrentPage}`;
+    const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`).then((response) => response.json());
+    let data = JSON.parse(response.contents);
+    setSearched(data.data.movies);
+  }
 
-    const [AllMovies, setAllMovies] = useState([]);
-    const [MovieCount, setMovieCount] = useState(0);
-    let param = useParams();
+  const handlePageClick = async (data) => {
+    let CurrentPage = data.selected + 1;
+    await fetchMovies(param.search, CurrentPage);
+  }
 
-
-
-    useEffect(() => {
-        const getFeaturedMovies = async (search) => {
-            const url = `https://yts.mx/api/v2/list_movies.json?query_term=${search}`;
-            const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`).then((response) => response.json());
-            let data = JSON.parse(response.contents);
-            console.log(data);
-            setAllMovies(data.data.movies);
-            setMovieCount(data.data.movie_count);
-        };
-        getFeaturedMovies(param.search);
-    }, [param.search]);
-
-
-
-    const fetchMovies = async (Search, CurrentPage) => {
-        const url = "https://yts.mx/api/v2/list_movies.json?query_term=" + Search + "&limit=20&page=" + CurrentPage
-        console.log(url);
-        const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`).then((response) => response.json());
-        let data = JSON.parse(response.contents);
-        console.log(data);
-        setAllMovies(data.data.movies);
-    }
-
-
-
-    const handlePageClick = async (data) => {
-        let CurrentPage = data.selected + 1;
-        console.log(CurrentPage);
-        const moviesFromServer = await fetchMovies(param.search, CurrentPage);
-    }
-
-
-
-    return (
-        <Wrapper>
-            <SearchBar />
-            <StyledPaginateContainer>
-                <ReactPaginate
-                    previousLabel={'Prev'}
-                    nexLabel={'Next'}
-                    onPageChange={handlePageClick}
-                    pageCount={MovieCount / 20}
-                    containerClassName={'pagination'}
-                    pageClassName={'pagination'}
-                    previousClassName={'pagination'}
-                    previousLinkClassName={'pagination__link'}
-                    nextLinkClassName={'pagination__link'}
-                    nextClassName={'pagination'}
-                    breakClassName={'pagination'}
-                    breakLinkClassName={'pagination'}
-                    disabledClassName={'pagination__link--disabled'}
-                    activeClassName={'pagination__link--active'}
-                    pageRangeDisplayed={1}
-                    marginPagesDisplayed={1}
-                />
-            </StyledPaginateContainer>
-            <Grid
-                animate={{ opacity: 1 }}
-                initial={{ opacity: 0 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.5 }}
-            >
-                {
-                    AllMovies.map((movie) => {
-                        return (
-                            <Card key={movie.id}>
-                                <Link to={/Movie/ + movie.id + "/" + movie.slug}>
-                                    <CardImage>
-                                        <img src={movie.large_cover_image} alt={movie.title} />
-                                    </CardImage>
-                                    <CardHover className="body">
-                                        <h2>{movie.genres[0]}</h2>
-                                        <button className="btn">View Movie</button>
-                                        <h4>{movie.runtime}mins</h4>
-                                    </CardHover>
-                                    <CardText>
-                                        <span className="quality">Full HD</span>
-                                        <div className="bottom">
-                                            <div className="moviename">
-                                                <span>{movie.year}</span>
-                                                <strong>{movie.title}</strong>
-                                            </div>
-                                            <div className="rating">
-                                                {movie.rating}/10
-                                                <img
-                                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/575px-IMDB_Logo_2016.svg.png"
-                                                    alt="IMDB Logo"
-                                                />
-                                            </div>
-                                        </div>
-                                    </CardText>
-                                </Link>
-                            </Card>
-                        );
-                    })
-                }
-            </Grid>
-        </Wrapper>
-    )
+  return (
+    <div className="wrapper">
+      <SearchBar />
+      <div className="pagination-container">
+        <ReactPaginate
+          previousLabel={'Prev'}
+          nextLabel={'Next'}
+          onPageChange={handlePageClick}
+          pageCount={MovieCount / 20}
+          containerClassName={'pagination'}
+          pageClassName={'pagination'}
+          previousClassName={'pagination'}
+          previousLinkClassName={'pagination__link'}
+          nextLinkClassName={'pagination__link'}
+          nextClassName={'pagination'}
+          breakClassName={'pagination'}
+          breakLinkClassName={'pagination'}
+          disabledClassName={'pagination__link--disabled'}
+          activeClassName={'pagination__link--active'}
+          pageRangeDisplayed={1}
+          marginPagesDisplayed={1}
+        />
+      </div>
+      <motion.div
+        className="grid"
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        {Searched.map((movie) => (
+          <div className="card" key={movie.id}>
+            <Link to={`/Movie/${movie.id}/${movie.slug}`}>
+              <div className="card-image">
+                <img src={movie.large_cover_image} alt={movie.title} />
+              </div>
+              <div className="card-hover body">
+                <h2>{movie.genres[0]}</h2>
+                <button className="btn">View Movie</button>
+                <h4>{movie.runtime}mins</h4>
+              </div>
+              <div className="card-text">
+                <span className="quality">Full HD</span>
+                <div className="bottom">
+                  <div className="moviename">
+                    <span>{movie.year}</span>
+                    <strong>{movie.title}</strong>
+                  </div>
+                  <div className="rating">
+                    {movie.rating}/10
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/575px-IMDB_Logo_2016.svg.png" alt="IMDB Logo" />
+                  </div>
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </motion.div>
+    </div>
+  )
 }
 
-const StyledPaginateContainer = styled.div`
-  .pagination {
-  display: flex;
-  justify-content: center;
-  list-style: none;
-  cursor: pointer;
-  padding: 1rem 0.2rem;
-}
-
-.pagination a {
-  padding: 10px;
-  border-radius: 0.3rem;
-  border: 1px solid #c5070a;
-
-  color: #c5070a;
-}
-
-.pagination__link {
-  font-weight: bold;
-}
-
-.pagination__link--active a {
-  color: #fff;
-  background: #c5070a;
-}
-
-.pagination__link--disabled a {
-  color: rgb(198, 197, 202);
-  border: 1px solid rgb(198, 197, 202);
-}
-`;
-const Grid = styled(motion.div)`
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
-    gap: 2rem;
-`;
-// ========== Setting Margin ==========
-const Wrapper = styled.div`
-  margin: 4rem 8rem;
-`;
-
-
-// ========== Slider Item Card ==========
-const Card = styled.div`
-  width: 200px;
-  height: 270px;
-  position: relative;
-  border-radius: 0.5rem;
-  border: 0.3rem solid white;
-  transition: all ease 0.3s;
-  &:hover {
-    border: 0.3rem solid #c5070a;
-  }
-
-  &:hover .body {
-    opacity: 1;
-  }
-`;
-
-// ==========  Card Image ==========
-const CardImage = styled.div`
-  width: 100%;
-  height: 100%;
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: fill;
-    object-position: center;
-  }
-`;
-
-// ==========  Card Text ==========
-const CardText = styled.div`
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(
-    360deg,
-    #161616b9 35%,
-    rgba(73, 73, 73, 0.23) 64%
-  );
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 25px;
-  justify-content: space-between;
-
-  // ==========  HD Label Text ==========
-  .quality {
-    background-color: #e70634;
-    color: #080808;
-    font-weight: 600;
-    padding: 0px 0.5rem;
-    height: 1.2rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-transform: uppercase;
-    letter-spacing: 0.4px;
-    border-radius: 4px;
-    opacity: 0;
-  }
-
-  // ==========  Movie Name ==========
-  .moviename {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-  }
-
-  .moviename strong {
-    font-size: 1rem;
-    line-height: 20px;
-    margin-top: 10px;
-    color: #dfdfdf;
-    letter-spacing: 0.5px;
-  }
-
-  // ==========  Rating ==========
-  .rating {
-    display: flex;
-    align-items: center;
-    justify-content: flex;
-    letter-spacing: 0.1rem;
-    img {
-      height: 2rem;
-      width: 2.4rem;
-      object-fit: contain;
-      object-position: center;
-      margin: 0px 10px;
-    }
-  }
-`;
-
-const CardHover = styled.div`
-  opacity: 0;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  z-index: 2;
-  top: 0;
-  left: 0;
-  transition: 0.5s;
-  padding: 10px;
-  background: linear-gradient(90deg, #000000b5 100%, #fafafa 0%);
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: column;
-  h2 {
-    margin-top: 30%;
-  }
-  h4 {
-    margin-bottom: 40%;
-  }
-  .btn {
-    border: none;
-    outline: 2px solid #d3060a;
-    background: transparent;
-    color: #d3060a;
-    cursor: pointer;
-    font-weight: bold;
-    padding: 5px 10px;
-    transition: 0.4s;
-  }
-  .btn:hover{
-    color: #a30507;
-    outline: 2px solid #a30507;
-  }
-`;
-
-
-export default AllMovies
+export default Searched;
