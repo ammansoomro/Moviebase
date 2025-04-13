5fc353599fdd42ca0ec2e5d31274c8e69b13c08c
Amman Soomro <63865428+ammansoomro@users.noreply.github.com>
2025-04-13 18:29:59 +0500

    Updated Navbar Styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "../Search/Search";
 import "./Navbar.scss";
 
 function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 10);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="nav bg-neutral-100 flex flex-align-center flex-justify-between py-base px-xl">
+    <div className={`nav bg-neutral-100 flex flex-align-center flex-justify-between py-base px-xl ${scrolled ? "scrolled" : ""}`}>
       <Link to="/">
         <div className="heading-2 blanka color-primary">MovieBase</div>
       </Link>
