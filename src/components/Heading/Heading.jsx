3a99a405efc61c6daa3260238b04f2095ed4a559
Amman Soomro <63865428+ammansoomro@users.noreply.github.com>
2025-04-13 16:45:54 +0500

    Updated Styling

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -5,7 +5,7 @@ function Heading() {
       <div className="flex-center flex-col gap-base pt-base px-xl">
         <div className='heading-1'>Download YTS YIFY movies: HD smallest size</div>
         <div className="highlight neutral-500 w-9-12 text-center">Welcome to the official MOVIEBASE website. Here you can browse and download movies in excellent 720p, 1080p, 2160p 4K and 3D quality, all at the smallest file size. YTS Movies Torrents.</div>
-        <div className='highlight-bold warning-300 text-center'>IMPORTANT - MOVIEBASE is developed using official YTS API</div>
+        <div className='highlight-bold color-primary text-center'>IMPORTANT - MOVIEBASE is developed using official YTS API</div>
       </div>
   );
 }
